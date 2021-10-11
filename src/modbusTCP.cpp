e7eb645aea4caf9f11b5be1d0e6474da96de3c5d
Maquine <mcmaquine@gmail.com>
2021-10-11 15:34:19 -0400

    push

--- src/modbusTCP.cpp
+++ src/modbusTCP.cpp
@@ -13,12 +13,12 @@
 using namespace std;
 
 modbus_t *mb;
-uint8_t bits[8];
-uint16_t input;
+uint8_t bits[16];//read buffer
+uint8_t coil[16];//write buffer
 //uint16_t tab_reg[32];
 
 
-uint16_t convert_raw_data( int nb, uint8_t *bits)
+uint16_t convert_raw_data_to_word( int nb, uint8_t *bits)
 {
 	uint16_t data = 0;
 
@@ -30,11 +30,56 @@ uint16_t convert_raw_data( int nb, uint8_t *bits)
 	return data;
 }
 
+//Return 1 when finish
+int convert_word_to_raw_data(int nb, uint16_t data, uint8_t *bits)
+{
+	for (int i = 0; i < nb; ++i)
+	{
+		bits[i] = data & 0x1;
+		data = data >> 1;
+	}
+
+	return 1;
+}
+
+int program()
+{
+	//if enters here, Factory I/O is running
+	modbus_read_input_bits(mb, 0, 16, bits);
+	coil[0] = 1;
+	modbus_write_bits(mb, 16, 16, coil);
+
+	do
+	{
+		if( bits[1] == 1 && bits[2] == 0 ) //sensor B esta detectando
+		{
+			coil[0] = 0;
+			coil[1] = 1;
+		}
+
+		if( bits[1] == 0 && bits[2] == 1) //sensor A esta detectando
+		{
+			coil[0] = 1;
+			coil[1] = 0;
+		}
+
+		modbus_write_bits(mb, 16, 16, coil);
+		modbus_read_input_bits(mb, 0, 16, bits);
+
+		if( bits[0] == 0 ) {
+			coil[0] = 0;
+			coil[1] = 0;
+			modbus_write_bits(mb, 16, 16, coil);
+			printf("factory I/O paused\n");
+		}
+	}while( bits[0] );
+
+	return 1;
+}
+
 int main() {
 
 	thread read;
-
-	int read_status;
 
 	mb = modbus_new_tcp("10.2.0.27", 502);
 
@@ -51,19 +96,16 @@ int main() {
 		return -1;
 	}
 
-	/*Read 1 bit*/
+	//Read First input to check if FACTIRY I/O is Running
+	modbus_read_input_bits(mb, 0, 1, bits);
 
-	cout << "Modbus Test" << endl; // prints "Hello Modbus"
-	read_status =  modbus_read_input_bits(mb, 0, 8, bits);
-	input = convert_raw_data(8, bits);
-
-	cout << "Modbus read inputs: " << read_status << endl;
-
-	printf("Inputs Hex: 0x%X\n", input);
-
-	for (int var = 0; var < 8; ++var) {
-		printf("Input[%d]: %d\n", var, bits[var]);
-	}
+	if( bits[0] )
+	{
+		//Running lock in while
+		printf("Factory I/O running\n");
+		program();
+	}else
+		printf("Factory I/O not running\n");
 
 	modbus_close( mb );
 	modbus_free( mb );
@@ -72,3 +114,17 @@ int main() {
 	cout	<<	"Exit"	<<	endl;
 	return 0;
 }
+
+/**
+ * cout << "Modbus Test" << endl; // prints "Hello Modbus"
+	read_status =  modbus_read_input_bits(mb, 0, 8, bits);
+	input = convert_raw_data_to_word(8, bits);
+
+	cout << "Modbus read inputs: " << read_status << endl;
+
+	printf("Inputs Hex: 0x%X\n", input);
+
+	for (int var = 0; var < 8; ++var) {
+		printf("Input[%d]: %d\n", var, bits[var]);
+	}
+ */
